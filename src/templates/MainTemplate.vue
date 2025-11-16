8a4ddc3420861feeb39a3ac32129a7690d2e9864
unknown <juancarlosmayorga1994@hotmail.com>
2025-11-16 14:07:12 -0300

    separado los menmus de acuerdo la autenticacion, falta crear menus y mantenedores, y todos los servicios

--- src/templates/MainTemplate.vue
+++ src/templates/MainTemplate.vue
@@ -1,32 +1,46 @@
 <template>
   <v-app>
     <!-- Drawer lateral -->
-    <v-navigation-drawer v-model="drawer" :width="400" permanent app
-      style="background: linear-gradient(185deg, rgb(45 43 43) 0%, rgb(94 87 87) 100%);">
+    <v-navigation-drawer
+      v-model="drawer"
+      :width="400"
+      permanent
+      app
+      style="background: linear-gradient(185deg, rgb(45 43 43) 0%, rgb(94 87 87) 100%);"
+    >
       <SelectedCountry />
-      <SideMenuPublic />
+      <SideMenuPublic v-if="authStore.isAuthenticated" />
 
       <template #append>
-        <div class="pa-2 text-white cursor-pointer">
-          Cerrar Sesión
-        </div>
+        <v-list-item @click="handleLogout" link>
+          <template v-slot:prepend>
+            <v-icon>mdi-logout</v-icon>
+          </template>
+          <v-list-item-title class="pa-2 text-white cursor-pointer">Cerrar Sesión</v-list-item-title>
+        </v-list-item>
       </template>
     </v-navigation-drawer>
 
     <!-- App bar superior -->
-    <v-app-bar app style="background: linear-gradient(90deg, rgba(99,102,241,1) 0%, rgba(67,56,202,1) 100%);">
+    <v-app-bar
+      app
+      style="background: linear-gradient(90deg, rgba(99,102,241,1) 0%, rgba(67,56,202,1) 100%);"
+    >
       <template #prepend>
         <v-app-bar-nav-icon color="white" @click="drawer = !drawer" />
       </template>
 
       <v-toolbar-title class="text-white font-weight-bold d-flex align-center">
-        <v-icon color="white" class="mr-2">mdi-certificate</v-icon>
-        TITULO
+        <v-icon color="white" class="mr-2">mdi-certificate</v-icon>TITULO
       </v-toolbar-title>
 
       <template #append>
         <span class="mr-4">
-          <ModalLogin />
+          <v-list-item v-if="authStore.isAuthenticated">
+            <v-list-item-title>{{ authStore.user?.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ authStore.user?.email }}</v-list-item-subtitle>
+          </v-list-item>
+          <ModalLogin v-else/>
         </span>
       </template>
     </v-app-bar>
@@ -41,18 +55,27 @@
     <!-- Footer -->
     <v-footer app>
       <v-col class="text-center" cols="12">
-        {{ new Date().getFullYear() }} — <strong>&copy;</strong>
+        {{ new Date().getFullYear() }} —
+        <strong>&copy;</strong>
       </v-col>
     </v-footer>
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import ModalLogin from '@/components/general/ModalLogin.vue'
-import SelectedCountry from '@/components/sideMenus/SelectedCountry.vue'
-import SideMenuPublic from '@/components/sideMenus/SideMenuPublic.vue'
+import { ref } from "vue";
+import ModalLogin from "@/components/general/ModalLogin.vue";
+import SelectedCountry from "@/components/sideMenus/SelectedCountry.vue";
+import SideMenuPublic from "@/components/sideMenus/SideMenuPublic.vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/store/auth";
 
-// Estado reactivo
-const drawer = ref(true)
+const router = useRouter();
+const authStore = useAuthStore();
+const drawer = ref(true);
+
+const handleLogout = async () => {
+  await authStore.logout();
+  router.push({ name: 'HomePublic' })
+};
 </script>
